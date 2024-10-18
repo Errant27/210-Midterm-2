85b4455334727f07ba78958cb3680ee2cb2f66e3
Errant27 <temporal.cerauno@gmail.com>
2024-10-17 18:28:37 -0700

    Initial Commit

---
+++ 210-Midterm-2/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  210-Midterm-2
+//
+//  Created by Neil Orton on 10/17/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
